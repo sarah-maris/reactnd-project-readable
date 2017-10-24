709dd2131fa31d0878bbf2c060904b023fc0c8b4
Sarah Maris <sarah@sarahmaris.com>
2017-10-24 16:21:36 -0500

    Populate data in post edit form

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react'
 import PostSidebar from './PostSidebar'
 import PostMain from './PostMain'
+import PostForm from './PostForm'
 import Comments from './Comments'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux';
-import { loadPostComments, sendComment } from '../actions'
+import { connect } from 'react-redux'
+import { loadPostComments, sendComment, sendPost } from '../actions'
 import AddComment from './AddComment'
-import { reset } from 'redux-form';
+import { reset } from 'redux-form'
+import Modal from 'react-modal'
 
 class Post extends Component {
   static propTypes = {
     post: PropTypes.object.isRequired,
   }
 
+  state = {
+    postModalOpen: false
+  }
+
   componentDidMount() {
     this.props.loadComments(this.props.post.id);
+  }
+
+  openEditPostModal = () => this.setState(() => ({ postModalOpen: true }))
+  closeEditPostModal = () => this.setState(() => ({ postModalOpen: false }))
+
+  editPost = (post) => {
+    // to create unique IDs for new posts
+    const uuidv1 = require('uuid/v1');
+    const updatedPost = {
+      id: uuidv1(),
+      timestamp: Date.now(),
+      title: post.title,
+      body: post.body,
+      author: post.author,
+      category: post.category
+    }
+    this.props.updatePost(updatedPost)
+    this.props.resetPostForm()
+    this.closeEditPostModal()
   }
 
   submitComment = (post) => {
@@ -33,18 +58,33 @@ class Post extends Component {
   render() {
     const {post, comments} = this.props
     const postComments = comments[post.id] || []
+    const { postModalOpen } = this.state
 
     return (
-    <article className="post">
-      <PostSidebar post={post} />
-      <div className="post-main">
-        <PostMain post={post} />
-        <Comments
-          postComments={postComments}
-          postId={post.id} />
+      <div>
+        <Modal
+          className='modal'
+          overlayClassName='overlay'
+          isOpen={postModalOpen}
+          onRequestClose={this.closeAddPostModal}
+          contentLabel='Modal'
+        >
+          {postModalOpen && <PostForm
+            initialValues={post}
+            onSubmit={this.editPost} />}
+        </Modal>
+        <article className="post">
+          <PostSidebar post={post} />
+          <div className="post-main">
+            <PostMain post={post} />
+            <Comments
+              postComments={postComments}
+              postId={post.id} />
+          </div>
+          <AddComment onSubmit={this.submitComment} />
+          <button onClick={this.openEditPostModal} className="add-button">Edit Post</button>
+        </article>
       </div>
-      <AddComment onSubmit={this.submitComment} />
-    </article>
     )
   }
 }
@@ -57,7 +97,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     loadComments: (postId) => dispatch(loadPostComments(postId)),
     addNewComment: (comment) => dispatch(sendComment(comment)),
-    resetCommentForm:() => dispatch(reset('addCommentForm'))
+    resetCommentForm:() => dispatch(reset('addCommentForm')),
+    updatePost: (post) => dispatch(sendPost(post)),
+    resetPostForm:() => dispatch(reset('addPostForm'))
   }
 }
 
